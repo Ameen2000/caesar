6efb7f9b78f10da294529b408256b3155660f7ec
Ameen2000 <Ameen2000@users.noreply.github.com>
2023-08-07 14:55:21 +0100

    Change exception into a normal print result to prevent crashing

--- bin/main.ml
+++ bin/main.ml
@@ -16,11 +16,11 @@ let () =
     | _ -> "Invalid input the only two commands are encrypt and decrypt"
   in
   let flag =
-    match List.nth args 4 with 
+    match List.nth args 4 with
     | Some "apply" -> Out_channel.write_all file ~data:result
     | Some x ->
-        let error_msg = "Unknown command " ^ x ^ " did you mean apply?"  in
-        raise (Invalid_argument error_msg)
+        let error_msg = "Unknown command " ^ x ^ " did you mean apply?\n" in
+        print_string error_msg
     | None -> ()
   in
   flag;
